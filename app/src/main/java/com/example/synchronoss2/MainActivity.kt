c56515a77cc648bed40084c23d88c4f80a0be022
rumaan <rummaanumme@gmail.com>
2022-09-06 10:30:19 +0530

    menus

--- app/src/main/java/com/example/synchronoss2/MainActivity.kt
+++ app/src/main/java/com/example/synchronoss2/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.synchronoss2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         super.onOptionsItemSelected(item)
+        when (item.itemId){
+            R.id.mi_settings -> {
+                Toast.makeText(this,   "opening settings",Toast.LENGTH_SHORT).show()
+            }
+            R.id.milogout -> {
+                Toast.makeText(this,   "logging out",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        return true
     }
 }
